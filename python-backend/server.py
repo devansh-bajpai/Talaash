0d4bf33ea6632f86cdc630f2ed64457f785e63ae
Devansh Bajpai <devanshb.code@gmail.com>
2025-12-06 17:35:54 +0530

    added server file

---
+++ python-backend/server.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from sentence_transformers import SentenceTransformer
+from pydantic import BaseModel
+from extractCaseDetails import extract_case_details
+from tools import dictToString
+from indexController import searchIndex
+
+
+class Case(BaseModel):
+    description: str
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 App starting...")
+    app.state.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    yield
+    
+    del app.state.model
+    print("⏬ App shutting down...")
+
+
+app = FastAPI(lifespan=lifespan)
+
+# @app.post("/haha")
+# def home(case: Case):
+#     return case.description + "hakgufebgha"
+
+
+@app.post('/similar')
+def getSimilarCases(case: Case):
+    case_description = case.description
+    case_dict = extract_case_details(case_description)
+
+    if(case_dict == None):
+        return None
+    
+    case_string = dictToString(case_dict)
+
+    model = app.state.model
+    embedding = model.encode([case_string])
+
+    idx, distances = searchIndex(embedding)    
+
+    # print(idx)
+    # print(distances)
+
+    return {
+        "indices": idx.flatten().tolist(),
+        "distances": distances.flatten().tolist()
+    }
